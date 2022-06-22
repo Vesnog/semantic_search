7799a6053d08b0d36648b79c8ad513c7ca1f1119
Ongun Arisev (desktop) <ongunarisev@gmail.com>
2022-06-22 22:22:51 +0200

    Load the word vectors
    + Word vectors in numpy array
    + Word names types in another numpy array
    - Encoding issues

--- load_NLPL_model.py
+++ load_NLPL_model.py
@@ -1,6 +1,19 @@
-import gensim
+import numpy as np
+import pandas as pd
 
 with open("./word_vec_rep/model.txt", "r") as file:
-    for line in file:
-        print(line)
-        input("Press to continue")
+    # Read the first line to get the dimensions of the vocabulary and word vector
+    first_line = file.readline()
+    dims = first_line.replace('\n', '').split(' ')
+    dims = np.array(dims, dtype=int)
+    # Allocate the memory in numpy
+    words_vec = np.zeros((dims[0], dims[1]), dtype=np.float32)
+    words_name_type = np.zeros((dims[0], 2), dtype=str)
+    for idx, line in enumerate(file):
+        line_list = line.replace('\n', '').split(' ')
+        words_vec[idx, :] = line_list[1:]
+        words_name_type[idx, :] = line_list[0].split('_')
+        # input("Press to continue")
+
+# model = gensim.models.fasttext.load_facebook_model("./word_vec_rep/model.bin")
+# pass
